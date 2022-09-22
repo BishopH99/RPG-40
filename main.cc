5562aa1efd7c8f17d1ec18ffbc685bd419bf8a44
BishopH99 <Bishophockman1@gmail.com>
2022-09-21 20:38:04 -0700

    design of map done

--- main.cc
+++ main.cc
@@ -10,34 +10,35 @@
 #include <algorithm>
 using namespace std;
 
-vector<string> world_map = { //FIXME: finish the world map
-	"*************************************************************************************************************************",
-	"* r   |                                                                                                                 *",
-	"*  s  |                                                                                                              d  *",
-	"*     |                                                                                                                 *",
-	"*   x ---                                                                                                               *",
-	"*        z                                                                                                              *",
-	"*      g                                                                                                                *",
-	"* d                                                                                                                     *",
-	"*    s r                                                                                                                *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*                                                                                                                       *",
-	"*************************************************************************************************************************",
+//Global Variable - accessible in all functions below
+vector<string> world_map = {
+	"*******************************************************************************",
+	"*     ___________           _________________________                         *",
+	"*    //_________\\\\      ___/                         \\_________________       *",
+	"*     |   ___   |______/                                               \\      *",
+	"*     |  |   |  |                 ___________                           \\     *",
+	"*     |  |___|  |______          /           \\         ___________       \\    *",
+	"*     |         |      \\        |    ___      \\        \\          \\      |    *",
+	"*     |__SMITH__|       |       |   /   \\______\\        \\         |______|    *",
+	"*                       |       |  |     |_______        |        /______\\    *",
+	"*______________________/        |   \\___/        \\       |        |      |    *",
+	"*                               |   WELL         |       |        |      |    *",
+	"*                               |                |       |        |STABLE|    *",
+	"*                               |   _____        |       |       _____        *",
+	"*______        ________         |  /     \\       |       |      /     \\_______*",
+	"*      \\      /        \\        |_/       \\      |       |     |              *",
+	"*      |      |         |                  \\     |       |     |     _________*",
+	"*      |______|         |            |?|   |     |        \\_____\\  /          *",
+	"*    _//      \\\\_       |            | |   |     |              |  |          *",
+	"*   //__________\\\\      |        _         /     |              |  |EVIL KING *",
+	"*    |   ____   |       |       | \\       /      |              |  |  CASTLE  *",
+	"*    |  |    |  |       |       |  \\_____/        \\_____________|  |          *",
+	"*    |  |____|  |      /       /                                /  \\__________*",
+	"*    |          |     /       /                                |              *",
+	"*    |__TAVERN__|    /       /                                 |       _______*",
+	"*                    |       |                                  \\_____/       *",
+	"*                    |       |                                                *",
+	"*******************************************************************************",
 };
 
 char get_world_location(size_t row, size_t col) {
@@ -53,11 +54,8 @@ void set_world_location(size_t row, size_t col, char c) {
 }
 
 void print_world(size_t player_row, size_t player_col) {
-	const auto [ROWS, COLS] = get_terminal_size();
-	int x = COLS / 2;
-	int y = ROWS / 2;
 	clearscreen();
-	movecursor(y, x);
+	movecursor(0, 0);
 	for (size_t row = 0; row < world_map.size(); row++) {
 		for (size_t col = 0; col < world_map.at(row).size(); col++) {
 			if (row == player_row and col == player_col) cout << '@';
@@ -67,78 +65,70 @@ void print_world(size_t player_row, size_t player_col) {
 		cout << endl;
 	}
 }
-/*  FIXME:code needed to add map to the game. (NOT ALL OF IT WORKS, IM FIGURING OUT HOW TO INTEGRATE IT TO OUR CODE)
 
-        if (get_world_location(row, col) == 'r') {
-            set_world_location(row, col, ' ');
-            movecursor(ROWS + 2, 0);
-            cout << "You picked up a radish!\n";
-        }
-        if (get_world_location(row, col) == 'z') {
-            movecursor(ROWS + 2, 0);
-            cout << "YOU WIN!!!!!!!!!^G^G^G\n";
-            usleep(2'000'000);
-            break;
-        }
-        if (c == ERR) usleep(1'000'000 / FPS);
-    }
-    set_raw_mode(false);
-    show_cursor(true);
-    movecursor(0, 0);
-    clearscreen();
-*/
 int main() {
-
-	const auto [ROWS, COLS] = get_terminal_size(); //How big the screen
-	const int world_ROWS = world_map.size();
-	const int world_COLS = world_map.at(0).size();
+	const int ROWS = world_map.size();
+	const int COLS = world_map.at(0).size(); //MAKE SURE ALL ROWS ARE THE SAME SIZE OR BAD TIMES
 	const int FPS = 60;
-	int world_row = world_ROWS / 2, world_col = world_COLS / 2;
-	int last_row = -1, last_col = -1;
-	int x = COLS / 2;
-	int y = ROWS / 2;
-
-	set_cursor_mode(false);
-	movecursor(y, x - 18);
-	clearscreen();
+	int row = ROWS / 2, col = COLS / 2;
+	int last_row = -1, last_col = -1; //Save our last position so we only redraw on update
 	set_raw_mode(true);
 	show_cursor(false);
 	while (true) {
-		int ch = quick_read();
-		cout << "🤴 Welcome To The Coding Crusade 👸" << endl;
-		cout << endl;
-		movecursor(y + 1, x - 18);
-		cout << "     press any key to continue" << endl;
-		if (cin.get()) break;
+		int c = toupper(quick_read());
+		if (c == 'Q') break;
+		if (c == 'W' or c == UP_ARROW) row--;
+		if (c == 'S' or c == DOWN_ARROW) row++;
+		if (c == 'A' or c == LEFT_ARROW) col--;
+		if (c == 'D' or c == RIGHT_ARROW) col++;
+		if (!(row == last_row and col == last_col)) { //If we moved...
+			print_world(row, col); //...redraw the map
+			last_row = row;
+			last_col = col;
+			movecursor(2, COLS + 5);
+			cout << BLUE << "ROW: " << row << RED << " COL: " << col << RESET;
+			movecursor(ROWS + 2, 0);
+			cout << "Welcome to the game\n";
+			cout.flush();
+		}
+		if (get_world_location(row, col) == 'r') {
+			set_world_location(row, col, ' ');
+			movecursor(ROWS + 2, 0);
+			cout << "You picked up a radish!\n";
+		}
+		if (get_world_location(row, col) == 'z') {
+			movecursor(ROWS + 2, 0);
+			cout << "YOU WIN!!!!!!!!!^G^G^G\n";
+			usleep(2'000'000);
+			break;
+		}
+		if (c == ERR) usleep(1'000'000 / FPS);
 	}
 	set_raw_mode(false);
-	clearscreen();
+	show_cursor(true);
 	movecursor(0, 0);
-	//Move a monster on screen.
-	//FIXME:add world map to game
-	setcolor(53, 97, 43);
-	set_raw_mode(true); //Nonblocking I/O
-	while (true) {
-		int ch = toupper(quick_read()); //Read from the keyboard
-		if (ch == 'Q') break;
-		if (ch == UP_ARROW or ch == 'W') y--;
-		if (ch == DOWN_ARROW or ch == 'S') y++;
-		if (ch == LEFT_ARROW or ch == 'A') x--;
-		if (ch == RIGHT_ARROW or ch == 'D') x++;
-		if (!(world_row == last_row and world_col == last_col)) { //checking movement
-			print_world(world_row, world_col); //redraw map
-			last_row = world_row;
-			last_col = world_col;
-			y = clamp(y, 0, ROWS - 2);
-			x = clamp(x, 0, COLS - 2);
-			movecursor(y, x);
-			cout << "🐰" << endl;
-		}
-		//clean up after yourself
-		set_raw_mode(false);
-		resetcolor();
-		show_cursor(true);
-		movecursor(0, 0);
-		clearscreen();
+	clearscreen();
+	/*
+	   cout << "Please enter a row and column" << endl;
+	   int row, col;
+	   cin >> row >> col;
+	   if (world_map.at(row).at(col) == '*') cout << "There is a wall there.\n";
+
+	//A vector is a data structure, "Something that holds multiple variables"
+	//Syntax: vector<type of thing to hold> name of the vector(size of the vector)
+	vector<int> help = {1,2,3,4,50};
+	//Print the contents of help:
+	for (int x : help) cout << x << endl;
+
+	//This just made 40 integers, within a vector named help
+	for (size_t i = 0; i < help.size(); i++) {
+	cout << "Please enter the value for help.at(" << i << ")\n";
+	cin >> help.at(i);
 	}
+	cout << "Here is the help vector!\n";
+	for (int x : help) {
+	cout << x << endl;
+	}
+	*/
 }
+
