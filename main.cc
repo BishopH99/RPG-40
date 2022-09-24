2e02bd780fa86d3a0e8bbce163d31da229b46719
BishopH99 <Bishophockman1@gmail.com>
2022-09-23 22:58:57 -0700

    TB functional and well entrance complete

--- main.cc
+++ main.cc
@@ -20,7 +20,7 @@ vector<string> world_map = {
 	"*     |  |___|  |______          /           \\         ___________       \\    *",
 	"*     |         |      \\        |    ___      \\        \\          \\      |    *",
 	"*     |__SMITH__|       |       |   /   \\______\\        \\         |______|    *",
-	"*                       |       |  |     |_______        |        /______\\    *",
+	"*                       |       |  |     <_______        |        /______\\    *",
 	"*______________________/        |   \\___/        \\       |        |      |    *",
 	"*                               |   WELL         |       |        |      |    *",
 	"*                               |                |       |        |STABLE|    *",
@@ -39,6 +39,21 @@ vector<string> world_map = {
 	"*                    |       |                                  \\_____/       *",
 	"*                    |       |                                                *",
 	"*******************************************************************************",
+};
+vector<string> well_map = {
+	"******************************",
+	"*   |_|                      *",
+	"*   |_|                      *",
+	"*   |_|     ___      ___     *",
+	"*   |_|    |   |    |   |    *",
+	"*   |_|    |___|    |___|    *",
+	"*   |_|                      *",
+	"*   |_|                      *",
+	"*   |_|                      *",
+	"*___|_|________________H____ *",
+	"*                            *",
+	"*                            *",
+	"******************************",
 };
 
 char get_world_location(size_t row, size_t col) {
@@ -66,10 +81,39 @@ void print_world(size_t player_row, size_t player_col) {
 	}
 }
 
+char get_well_location(size_t row, size_t col) {
+	if (row >= well_map.size()) return ' ';
+	if (col >= well_map.at(row).size()) return ' ';
+	return well_map.at(row).at(col);
+}
+
+void set_well_location(size_t row, size_t col, char c) {
+	if (row >= well_map.size()) return;
+	if (col >= well_map.at(row).size()) return;
+	well_map.at(row).at(col) = c;
+}
+
+
+void print_well(size_t player_row, size_t player_col) {
+	clearscreen();
+	movecursor(0, 0);
+	for (size_t row = 0; row < well_map.size(); row++) {
+		for (size_t col = 0; col < well_map.at(row).size(); col++) {
+			if (row == player_row and col == player_col) cout << "@";
+			else
+				cout << well_map.at(row).at(col);
+		}
+		cout << endl;
+	}
+}
+
+
 int main() {
 	const int ROWS = world_map.size();
 	const int COLS = world_map.at(0).size(); //MAKE SURE ALL ROWS ARE THE SAME SIZE OR BAD TIMES
 	const int FPS = 60;
+	int quest_accepted = 0;
+	int well_entrance = 0;
 	int Health = 50;
 	int row = ROWS / 2, col = COLS / 2;
 	int last_row = -1, last_col = -1; //Save our last position so we only redraw on update
@@ -92,6 +136,8 @@ int main() {
 			cout << BLUE << "ROW: " << row << RED << " COL: " << col << RESET;
 			movecursor(5, COLS + 5);
 			cout << RED  << "HP: " << WHITE << Health << endl; //FIXME: add color indictors to the HP
+			movecursor(8, COLS + 5);
+			cout << "Quest Tracker: " << quest_accepted;
 			movecursor(ROWS + 2, 0);
 			setcolor(255, 0, 0);
 			cout << "Welcome to the EVIL KING RECURSION'S DOMAIN\n";
@@ -136,13 +182,39 @@ int main() {
 			cout << BOLDYELLOW << "|";
 			movecursor(21, 65);
 			cout << BOLDYELLOW << "|";
+			if (quest_accepted > 0) {
+				movecursor(17, 39);
+				cout << BOLDBLACK << "?";
+			}
 			resetcolor();
 			cout.flush();
 		}
-		if (get_world_location(row, col) == 'r') {
-			set_world_location(row, col, ' ');
-			movecursor(ROWS + 2, 0);
-			cout << "You picked up a radish!\n";
+		if (get_world_location(row, col) == '<') {
+			movecursor(ROWS + 8, 0);
+			cout << "Do you want to enter the well? (Y/N)\n";
+			if (c == 'Y') {
+				well_entrance++;
+				break;
+			}
+		}
+		if (get_world_location(row, col) == '?') {
+			movecursor(ROWS + 8, 0);
+			cout << "There is a help request on the town board\n";
+			cout.flush();
+			movecursor(ROWS + 9, 0);
+			cout << "     do you wish to read it? (Y/N)\n";
+			if (c == 'Y') {
+				cout << RESET;
+				movecursor(ROWS + 11, 0);
+				cout << "The request says someone lost their cat around the stables\n";
+				movecursor(ROWS + 12, 0);
+				cout << "Do you want to accept the request from the villager? (Y/N)\n";
+				if (c == 'Y') {
+					set_world_location(row, col, ' ');
+					quest_accepted++;
+					continue;
+				}
+			}
 		}
 		if (get_world_location(row, col) == 'z') {
 			movecursor(ROWS + 2, 0);
@@ -156,6 +228,32 @@ int main() {
 	show_cursor(true);
 	movecursor(0, 0);
 	clearscreen();
+	set_raw_mode(true);
+	show_cursor(false);
+	while (true) {
+		const int ROWS = well_map.size();
+		const int COLS = well_map.at(0).size(); //MAKE SURE ALL ROWS ARE THE SAME SIZE OR BAD TIMES
+		const int FPS = 60;
+		int row = ROWS / 2, col = COLS / 2;
+		int last_row = -1, last_col = -1; //Save our last position so we only redraw on update
+		while (well_entrance == 1) {
+			int c = toupper(quick_read());
+			if (c == 'Q') break;
+			if (c == 'W' or c == UP_ARROW) row--;
+			if (c == 'S' or c == DOWN_ARROW) row++;
+			if (c == 'A' or c == LEFT_ARROW) col--;
+			if (c == 'D' or c == RIGHT_ARROW) col++;
+			row = clamp(row, 1, ROWS - 2);
+			col = clamp(col, 1, COLS - 2);
+			if (!(row == last_row and col == last_col)) { //If we moved...
+				print_well(row, col); //...redraw the map
+				last_row = row;
+				last_col = col;
+			}
+		}
+		resetcolor();
+		cout.flush();
+	}
 	/*
 	   cout << "Please enter a row and column" << endl;
 	   int row, col;
